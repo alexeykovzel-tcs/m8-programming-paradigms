6eace21fc994737bdc7c7f0e59f1992114fcc9f6
Alexey Kovzel <alexey.kovzel@gmail.com>
2023-05-12 15:03:17 +0200

    Do some exercises for FP block 3

--- src/block3/fp/Set5.hs
+++ src/block3/fp/Set5.hs
@@ -3,12 +3,11 @@ module Set5 where
 import Data.List
 import Data.Functor
 import Data.Either
+import Test.QuickCheck
 
 import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Language
 import qualified Text.ParserCombinators.Parsec.Token as Token
-
-import Test.QuickCheck
 
 fromLeft' :: Either l r -> l
 fromLeft' (Left x) = x -- Newer GHC versions contain a fromLeft :: l -> Either l r -> l
@@ -18,21 +17,106 @@ fromRight' (Right x) = x -- Newer GHC versions contain a fromRight :: r -> Eithe
 
 parser :: Parser a -> String -> a
 parser p xs | isLeft res = error $ show $ fromLeft' res
-          | otherwise  = fromRight' res
+            | otherwise  = fromRight' res
   where res = parse p "" xs
 
 
--- Uncomment for later exercises:
-{-
--- Expression EDSL
-data Expr = Const Integer
-          | Var String
-          | Mult Expr Expr
-          | Add Expr Expr
-          deriving Show
--}
-{-
-fib :: Integer -> Integer
-fib = (evalfun . parserFun)
+data Expr 
+    = Const Integer
+    | Var String
+    | Mult Expr Expr
+    | Add Expr Expr
+    | If Cond Expr Expr
+    | FunCall String Expr
+    | Dec Expr 
+    deriving Show
+
+
+-- Exercise 2
+
+languageDef = emptyDef {
+    Token.reservedOpNames = ["+", "*"]
+}
+
+lexer = Token.makeTokenParser languageDef
+
+identifier = Token.identifier lexer
+parens = Token.parens lexer
+reserved = Token.reserved lexer
+symbol = Token.symbol lexer
+num = Token.integer lexer
+
+
+-- Exercise 3
+
+parseExpr :: Parser Expr
+parseExpr = 
+    parseDec 
+    <|> parseIf
+    <|> ((\a b -> foldl Add a b) 
+        <$> parseTerm <*> (many $ symbol "+" *> parseTerm))
+    <?> "expression"
+
+parseFactor :: Parser Expr
+parseFactor = 
+    (Const <$> num) 
+    <|> (parens parseExpr)
+    <|> try(FunCall <$> identifier <*> (parens parseExpr)) 
+    <|> (Var <$> identifier) 
+    <?> "id or number"
+
+parseTerm :: Parser Expr
+parseTerm = 
+    (\a b -> foldl Mult a b) 
+    <$> parseFactor <*> (many $ symbol "*" *> parseFactor)
+    <?> "term (e.g. 2*3*5)"
+
+
+-- Exercise 4
+
+data Cond 
+    = Eq Expr Expr
+    deriving Show
+
+parseCond :: Parser Cond
+parseCond = Eq 
+    <$> parseExpr <*> (string "==" *> parseExpr)
+    <?> "condition"
+
+parseDec :: Parser Expr
+parseDec = Dec 
+    <$> (symbol "dec" *> parseExpr)
+    <?> "dec (wtf is it)"
+
+parseIf :: Parser Expr
+parseIf = If 
+    <$> (symbol "if" *> parseCond) 
+        <*> (symbol "then" *> parseExpr) 
+        <*> (symbol "else" *> parseExpr)
+
+
+-- Exercise 5
+
+data FunDef 
+    = Func String String Expr
+    deriving Show
+
+parseFun :: Parser FunDef
+parseFun = Func 
+    <$> (symbol "function" *> identifier)
+        <*> identifier
+        <*> (symbol "=" *> parseExpr)
+
+fib :: FunDef
+fib = parser parseFun
  "function fib x = if x == 0 then 1 else (if x == 1 then 1 else fib(dec x)+fib(dec dec x))"
--}
+
+
+-- Exercise 6
+
+-- evalCond :: Cond ->
+
+-- evalExpr :: Expr -> 
+
+evalFun :: FunDef -> Integer -> Integer
+evalFun f x = 0
